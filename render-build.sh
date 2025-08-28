443e7f24f09f53137d91b8a050be3e2bfb040fda
Fedlallah Mohamed Yacine <127243578+fmyacine@users.noreply.github.com>
2025-08-28 02:34:12 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-# fail on error
 set -o errexit
 
-# Install Rust (needed for cryptography/orjson/etc.)
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-# Then install Python deps
+# Upgrade pip & install deps
 pip install --upgrade pip
 pip install -r requirements.txt
